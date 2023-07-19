813fa8fa1ab8a18b02c7c010ae266ab37a5369c6
xeonds <1456506329@qq.com>
2023-07-20 04:59:57 +0800

    Update about

--- frontend/src/views/about.vue
+++ frontend/src/views/about.vue
@@ -1,10 +1,34 @@
 <template>
-  <el-text><h2>About</h2></el-text>
+  <el-divider>About</el-divider>
+  <p>
+    <el-text> 一个简单的RSS聚合站。 </el-text>
+  </p>
   <p>
     <el-text>
-      一个简单的RSS聚合站。<br />
       共同写作，共同阅读，共同分享。这就是这个站点存在的意义。
     </el-text>
   </p>
-  <p><el-text>目前没怎么做UI，只是先把主要功能完成了而已。</el-text></p>
+  <p>
+    <el-text
+      >本来是出给XDU-ISC
+      Web组的练手项目，结果最后还是我一个人做完了（笑）</el-text
+    >
+  </p>
+  <p>
+    <el-text>不过也好，作为一个聚合站，能把大家都聚起来就好。</el-text>
+  </p>
+  <el-divider>Update log</el-divider>
+  <p>
+    <el-text>
+      2023.7.19
+      看到szgg的XDU-Planet才记起来我这个同名的玩意已经一年多没更新了（<br />
+      诈尸更新一下（溜
+    </el-text>
+  </p>
 </template>
+
+<style>
+p {
+  text-indent: 1.25em;
+}
+</style>
